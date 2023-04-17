065f3afb55677d2f1c9d67810a839195cd45a6dc
Westsi <76999267+Westsi@users.noreply.github.com>
2023-04-17 11:46:13 +0100

    updated cnm to add timestamp at init

--- cnm.go
+++ cnm.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"time"
 )
 
 func createFile(path string, contents string) {
@@ -13,14 +14,24 @@ func createFile(path string, contents string) {
 }
 
 func main() {
+	t_s := getDTC()
 	dir := os.Args[1]
 
 	// make directory
 	os.Mkdir(dir, 0755)
 
 	// make header file
-	createFile(fmt.Sprintf("%s/%s.h", dir, dir), fmt.Sprintf("#ifndef %s_H_\n#define %s_H_\n\n#endif", strings.ToUpper(dir), strings.ToUpper(dir)))
+	createFile(fmt.Sprintf("%s/%s.h", dir, dir), fmt.Sprintf("/* date = %s */\n\n#ifndef %s_H_\n#define %s_H_\n\n#endif //%s_H", t_s, strings.ToUpper(dir), strings.ToUpper(dir), strings.ToUpper(dir)))
 
 	// make source file
-	createFile(fmt.Sprintf("%s/%s.c", dir, dir), fmt.Sprintf("#include \"%s.h\"", dir))
+	createFile(fmt.Sprintf("%s/%s.c", dir, dir), fmt.Sprintf("/* date = %s */\n\n#include \"%s.h\"", t_s, dir))
+}
+
+func getDTC() string {
+	// returns a string in format "17 Apr 2022 13:29"
+	// monthtexts := []string{"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"}
+
+	t := time.Now()
+	t_s := t.Format("02 Jan 2006 15:04")
+	return t_s
 }
